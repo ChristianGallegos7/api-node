ff6076bd0bf6d83fa5279f295159ea001d88a6d7
Christian <christiantenz7@gmail.com>
2023-09-19 21:14:03 -0500

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import express from 'express'
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+import { PORT } from './config.js'
+
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 1234
 
 app.use(indexRoutes)
 app.use('/api', employeesRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Ruta no encontrada'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`)
