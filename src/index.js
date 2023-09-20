73f50028cd473092df962ddd6571deb43b12deb8
Christian <christiantenz7@gmail.com>
2023-09-19 21:19:15 -0500

    second step

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
-
+import app from './app.js'
 import { PORT } from './config.js'
-
-const app = express()
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', employeesRoutes)
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Ruta no encontrada'
-  })
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`)
